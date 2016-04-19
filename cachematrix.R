1e7f58a91f7a6fa8a1a8afc5449ab4b2ac251a1f
staszz2 <bea_stas@yahoo.com>
2016-04-19 16:09:04 -0500

    Submit
    version 1.00

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,73 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions store new matrix object, calculate the inverse,
+## and store it in cache
+##
+## Example
+##
+## > x = matrix(1:4,2,2)
+## > matrixObject <- makeCacheMatrix(x)
+## > cacheSolve(matrixObject)
+## calculating new inversed result
+## [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+## > cacheSolve(matrixObject)
+## getting cached matrix
+## [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
 
-## Write a short comment describing this function
+
+
+## makeCacheMatrix(x) populates an object with new matrix and 
+##stores cache of its inverse
+## object properties:
+##      get()
+##      set(x)
+##      getinverse() 
+##      setinverse(xx)
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  invMatrix <- NULL
+  
+  set <- function(y) { #set new object and clear cache
+    x <<- y
+    invMatrix <<- NULL
+  }
+  
+  get <- function() x #return the original matrix
+  
+  setinverse <- function(solve) invMatrix<<- solve #set inverse cache
+  
+  getinverse <- function() invMatrix               #get inverse cache
+  
+  list(set = set, get = get, getinverse = getinverse, setinverse = setinverse)
 }
 
 
-## Write a short comment describing this function
+## This function returns inverse of the matrix. 
+## If cached result is available, it returns cache
+## If cache is empty , it calculates new result and stores it in cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  #attempt to get cache
+  invMatrix <- x$getinverse()
+  
+  #if cache exists, return it
+  if(!is.null(invMatrix)) {
+    message("getting cached matrix")
+  }
+  #if cache is null, calculate new result and cache it
+  else
+  {
+    message("calculating new inversed result")
+    data <- x$get()
+    invMatrix <- solve(data, ...)
+    x$setinverse(invMatrix)
+  }
+  
+  return(invMatrix)
 }
+
+
